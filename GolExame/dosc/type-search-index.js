efea2dec2973b44ff26f71360b68721e3c96fb9b
Gabriel Moura <123528138+GabrielKKKJJ@users.noreply.github.com>
2024-03-19 18:42:16 -0300

    Delete GolExame/dosc directory

--- GolExame/dosc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"gol","l":"Generations"},{"p":"gol","l":"Main"},{"p":"gol","l":"Render"},{"p":"gol","l":"Settings"},{"p":"gol","l":"ValidarArgs"}];updateSearchResults();
